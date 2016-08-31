72e50d28f478e1dbeab1e412ecdc7f0e860affb1
stoege <mail@stoege.ch>
2016-08-31 23:46:55 +0200

    Update ansible.sh

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -6,18 +6,12 @@ if [ x$VAGRANT_ANSIBLE = x'yes' ]; then
 	. /root/.profile
 
 	uname_r=`uname -r`
-	if [ $uname_r = 5.9 ]; then
+	if [ $uname_r = 6.0 ]; then
+		pkg_add python-2.7.12
+	elif [ $uname_r = 5.9 ]; then
 		pkg_add python-2.7.11
 	elif [ $uname_r = 5.8 ]; then
 		pkg_add python-2.7.10
-	elif [ $uname_r = 5.7 ]; then
-		pkg_add python-2.7.9
-	elif [ $uname_r = 5.6 ]; then
-		pkg_add python-2.7.8
-	elif [ $uname_r = 5.5 ]; then
-		pkg_add python-2.7.6p0
-	elif [ $uname_r = 5.4 ]; then
-		pkg_add python-2.7.5
 	else
 		echo "Did not recognise OpenBSD version."
 		exit 1
